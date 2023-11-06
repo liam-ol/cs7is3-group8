c78f7c2ab6aefdbbe326ca1eabb222a8a4ce5334
George Kanellopoulos <kanellog@tcd.ie>
2023-11-06 21:58:16 +0000

    Extended main to create an instance of TopicParser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+package search.engine;
+
 import org.apache.lucene.*;
 import java.io.IOException;
 import java.nio.file.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
+        TopicParser tparser = new TopicParser();
+        System.out.println("The following topics have been parsed:");
+        for (Topic top: tparser.topics) {
+            System.out.print(top.id + ", ");
+        }
+        System.out.println();
     }
 }
