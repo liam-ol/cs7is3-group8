31337a1d66e79746ce1887a293b072a7d667f6bd
George Kanellopoulos <kanellog@tcd.ie>
2023-11-06 21:57:33 +0000

    Moved Topic class to a separate file

---
+++ src/main/java/Topic.java
@@ -0,0 +1,16 @@
+package search.engine;
+
+// The Topic class holds all information about a topic.
+public class Topic {
+    int id;
+    String title;
+    String description;
+    String narrative;
+
+    public Topic(int id, String title, String description, String narrative) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.narrative = narrative;
+    }
+}
