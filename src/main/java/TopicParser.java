1886f80ae25878837a70b7111114688d40bb6f41
George Kanellopoulos <kanellog@tcd.ie>
2023-11-06 21:57:49 +0000

    Finished readTopics function

--- src/main/java/TopicParser.java
+++ src/main/java/TopicParser.java
@@ -1,4 +1,11 @@
+package search.engine;
+
 import java.util.ArrayList;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import java.lang.StringBuilder;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,35 +13,73 @@ import java.io.BufferedReader;
 
 public class TopicParser {
 
-    private class Topic {
-        String title;
-        String description;
-        String narrative;
-    }
-
+    public ArrayList<Topic> topics;
+    private String _TOPICS_FILE = "./topics";
+    
+    // readTopics function reads the topics from a file and stores them in an ArrayList.
     private ArrayList<Topic> readTopics() {
 
-        String _TOPICS_FILE = "/home/azureuser/cs7is3-group8/data/topics";
-
-        String line;
         ArrayList<Topic> topics = new ArrayList<Topic>();
-        Topic top = new Topic();
+        
+        String line;
         FileReader fr = null;
         BufferedReader br = null;
+        StringBuilder topicText = null;
 
         try {
+            // Open the topics file.
             fr = new FileReader(_TOPICS_FILE);
             br = new BufferedReader(fr);
+
+            // Read the file line to line.
             line = br.readLine();
             while (line != null) {
+                // For every topics read all lines and save them in a string variable.
+                if (line.contains("<top>")) {
+                    topicText = new StringBuilder();
+                    line = br.readLine();
+                    while (!line.contains("</top>")) {
+                        if (line != "\n") {
+                            topicText.append(line.trim() + " ");
+                        }
+                        line = br.readLine();
+                    }
+
+                    // Isolate number, title, description and narrative using a regular expression.
+                    Pattern pattern = Pattern.compile("^\\s*<num>\\s*Number:\\s*(?<number>\\d+)\\s*<title>(?<title>.*?)<desc>\\s*Description:(?<description>.*?)<narr>\\s*Narrative:(?<narrative>.*)");
+                    Matcher matcher = pattern.matcher(topicText.toString());
+
+                    // Save the topic info in an instance of Topic class and add them to the topics list.
+                    if (matcher.find()) {
+                        int id = Integer.parseInt(matcher.group("number"));
+                        String title = matcher.group("title").trim();
+                        String descr = matcher.group("description").trim();
+                        String narr = matcher.group("narrative").trim();
+                        Topic currTopic = new Topic(id, title, descr, narr);
+                        topics.add(currTopic);
+                    }
+                }
                 line = br.readLine();
             }
-            System.out.println("All lines read!");
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+        finally {
+            // Close the file gracefully.
+            try {
+                br.close();
+                fr.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return topics;
+    }
+
+    public TopicParser() {
+        this.topics = readTopics();
     }
 
 }
