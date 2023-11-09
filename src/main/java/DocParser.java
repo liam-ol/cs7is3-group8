12743571dc9d1732cbb4db92fe627d742123c595
Liam <lolionai@tcd.ie>
2023-11-09 10:07:18 +0000

    Example parser function for FR94

--- src/main/java/DocParser.java
+++ src/main/java/DocParser.java
@@ -1,4 +1,8 @@
 import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
 
 public class DocParser {
 
@@ -12,20 +16,43 @@ public class DocParser {
         return docFile.trim().split("(?<=/DOC>)\\n");
     }
 
-    /* TODO: add parsing functions for each doc type
-    public static Doc parseFBIS() {
+    public static Doc parseFR94(String docRaw, DocumentBuilder parser) throws IOException, SAXException {
+        String docTitle, docSummary, docBody;
+        // First some bulk replaces: remove all comments and &blank;s
+        String docClean = docRaw.replaceAll("<!--.*-->\\n", "")
+                .replaceAll("&blank;", " ")
+                .replaceAll("&hyph;", "-");
+
+        // TODO: Set up a DocumentBuilder instance in Main - we can pass it into these functions and save time.
+        Document doc = parser.parse(docClean);
+
+        // All docs have a <DOCID> and <TEXT>.
+        int docID = Integer.parseInt(doc.getElementsByTagName("DOCID").item(0).getTextContent());
+        docBody = doc.getElementsByTagName("TEXT").item(0).getTextContent();
+
+        // For other tags, check if they exist first.
+        NodeList title = doc.getElementsByTagName("DOCTITLE");
+        if (title.getLength() > 0)
+            docTitle = title.item(0).getTextContent();
+        else docTitle = "";
+        NodeList summary = doc.getElementsByTagName("SUMMARY");
+        if (summary.getLength() > 0)
+            docSummary = summary.item(0).getTextContent();
+        else docSummary = "";
+
+        return new Doc(docID, docTitle, "", docSummary, docBody);
+    }
+
+    /* TODO: add parsing functions for each other doc type
+    public static Doc parseFBIS(String docRaw, DocumentBuilder parser) throws IOException, SAXException  {
 
     }
 
-    public static Doc parseFR94() {
+    public static Doc parseFT(String docRaw, DocumentBuilder parser) throws IOException, SAXException  {
 
     }
 
-    public static Doc parseFT() {
-
-    }
-
-    public static Doc parseLATimes() {
+    public static Doc parseLATimes(String docRaw, DocumentBuilder parser) throws IOException, SAXException  {
 
     }
 
