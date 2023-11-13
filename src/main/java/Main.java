b06faca3934ced30dc62e6482ab59db3302b1c1d
Liam <lolionai@tcd.ie>
2023-11-13 15:19:42 +0000

    Document parsing test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,21 @@
-//package search.engine;
-
 import org.apache.lucene.*;
-import java.io.IOException;
+
+import javax.xml.parsers.*;
+
 import java.nio.file.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        /* DOCUMENT PARSING */
+        // This DocumentBuilder instance is passed to every parse function call, saving resources.
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+        // Test parsing of a document from the FR94 set.
+        String[] testDocsRaw = DocParser.getDocList(new String(Files.readAllBytes(Paths.get("./Assignment Two/fr94/01/fr940104.0"))));
+        Doc testDoc = DocParser.parseFR94(testDocsRaw[1], builder);
+        testDoc.print();
+
+        /* TOPIC PARSING */
         TopicParser tparser = new TopicParser();
         System.out.println("The following topics have been parsed:");
         for (Topic top: tparser.topics) {
