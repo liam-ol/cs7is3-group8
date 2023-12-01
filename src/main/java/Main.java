d25eb61a09b2eb11ccb6d04b100ef514282487fd
George Kanellopoulos <kanellog@tcd.ie>
2023-12-01 14:22:24 +0000

    Experiment mode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,13 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.index.DirectoryReader;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similarities.LMDirichletSimilarity;
 
 public class Main {
 
@@ -15,17 +19,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         /* SEARCH ENGINE PARAMETERS */
-        Analyzer analyzer = new EnglishAnalyzer();
+        Analyzer analyzer = new CustomAnalyzer();
         Similarity similarity = new BM25Similarity();
         Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
 
         /* DOCUMENT PARSING AND INDEXING */
         Indexer indexer = new Indexer(analyzer, similarity, directory);
-        if (DirectoryReader.indexExists(directory)) {
-            System.out.println("Index is already built.");
-        } else {
-            indexer.readAndIndexDocuments();
-        }
+        indexer.readAndIndexDocuments();
+        // if (DirectoryReader.indexExists(directory)) {
+        //     System.out.println("Index is already built.");
+        // } else {
+        // }
 
         indexer.shutDown();
 
